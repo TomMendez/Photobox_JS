e32a976775dcdd99be2349a6fec6f74396bdf357
TomMendez <tom.mendez-porcel5@etu.univ-lorraine.fr>
2020-03-10 15:02:33 +0100

    affichage de la lightbox en bas de la page OK

--- gallery.js
+++ gallery.js
@@ -1,5 +1,5 @@
 import * as photo from './photoloader.js';
-
+import * as lbox from './lightbox.js';
 
 let urls, id;
 
@@ -21,36 +21,8 @@ export let loadData = function () {
                 <div>` + p.photo.titre + `</div>
                 </div>`).appendTo($("#photobox-gallery"));
             }
-            $(".vignette").on("click",lightbox);
+            $(".vignette").on("click",lbox.lightbox);
 
         }
     )
-}
-
-export let lightbox = function(e) {
-    let uri = e.target.getAttribute("data-uri");
-    let promesse = axios.get(uri,{
-        withCredentials: true,
-        responseType: "json"
-    }).catch(() => "erreur lors du chargement de l'image");
-
-    promesse.then(
-        (img) => {
-            console.log(img); //DEBUG
-            $('<div class="lightbox_container" id="lightbox_container">\n' +
-                '        <div id="lightbox">\n' +
-                '            <div id="lightbox-head">\n' +
-                '                <p id="lightbox_close">-X-</p>\n' +
-                '                <h1 id="lightbox_title">' + img.data.photo.titre + '</h1>\n' +
-                '            </div>\n' +
-                '\n' +
-                '            <div id="lightbox-img">\n' +
-                '                <img id="lightbox_full_img" src="' + photo.server_url+img.data.photo.url.href + '">\n' +
-                '            </div>\n' +
-                '        </div>\n' +
-                '\n' +
-                '    </div>').appendTo($(".photobox-gallery"));
-        }
-    );
-
 }
