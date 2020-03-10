bfc98670ceac0c5926fe28f01e61f24457e18620
TomMendez <tom.mendez-porcel5@etu.univ-lorraine.fr>
2020-03-10 14:51:31 +0100

    ajout de la méthode lightbox basique

--- gallery.js
+++ gallery.js
@@ -1,5 +1,6 @@
 import * as photo from './photoloader.js';
 
+
 let urls, id;
 
 export let init = function () {
@@ -20,7 +21,36 @@ export let loadData = function () {
                 <div>` + p.photo.titre + `</div>
                 </div>`).appendTo($("#photobox-gallery"));
             }
+            $(".vignette").on("click",lightbox);
 
         }
     )
+}
+
+export let lightbox = function(e) {
+    let uri = e.target.getAttribute("data-uri");
+    let promesse = axios.get(uri,{
+        withCredentials: true,
+        responseType: "json"
+    }).catch(() => "erreur lors du chargement de l'image");
+
+    promesse.then(
+        (img) => {
+            console.log(img); //DEBUG
+            $('<div class="lightbox_container" id="lightbox_container">\n' +
+                '        <div id="lightbox">\n' +
+                '            <div id="lightbox-head">\n' +
+                '                <p id="lightbox_close">-X-</p>\n' +
+                '                <h1 id="lightbox_title">' + img.data.photo.titre + '</h1>\n' +
+                '            </div>\n' +
+                '\n' +
+                '            <div id="lightbox-img">\n' +
+                '                <img id="lightbox_full_img" src="' + photo.server_url+img.data.photo.url.href + '">\n' +
+                '            </div>\n' +
+                '        </div>\n' +
+                '\n' +
+                '    </div>').appendTo($(".photobox-gallery"));
+        }
+    );
+
 }
